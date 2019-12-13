c3abd034d180ccaa2bdecb8b187d1687dcea0e26
robin88322 <fedeyko88@gmail.com>
2019-12-13 15:48:45 +0200

    Fixed Bugs

--- bucket.tf
+++ bucket.tf
@@ -1,8 +1,8 @@
 terraform {
  backend "s3" {
  encrypt = true
- bucket = "${bucket_name}"
- region = "${var.aws_region}"
- key = terraform.tfstate
+ bucket = "my.tfstate.bucket"
+ region = "us-east-2"
+ key = "terraform.tfstate"
  }
 }
