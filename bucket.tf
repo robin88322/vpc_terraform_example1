3fdc3e1d645d8b6fc28ea76fb7f8c247dc020c67
robin88322 <fedeyko88@gmail.com>
2019-12-13 15:42:44 +0200

    Fixed Bugs and Added remote state

---
+++ bucket.tf
@@ -0,0 +1,8 @@
+terraform {
+ backend “s3” {
+ encrypt = true
+ bucket = "${bucket_name}"
+ region = "${var.aws_region}"
+ key = /terraform.tfstate
+ }
+}
--- variables.tf
+++ variables.tf
@@ -8,3 +8,7 @@ variable "cidr_subnets" {
   type    = "list"
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
+
+variable "bucket_name" {
+  default = "my.tfstate.bucket"
+}
